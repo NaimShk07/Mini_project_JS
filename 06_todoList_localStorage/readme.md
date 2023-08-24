4dd983b14b526d3a02db917ce1677862367f661a
naimshk07 <nayemuddinshaikh@gmail.com>
2023-08-24 11:37:35 +0530

    minor changes

--- 06_todoList_localStorage/readme.md
+++ 06_todoList_localStorage/readme.md
@@ -1,6 +1,7 @@
 ## something new
 
+1. resize:vertical //it will stop textarea from expanding horizontally
 1. task.splice(index, 1); //because this code is in foreach loop it will delete on which it click
-2. local storage accept data in json format , so when ever we send or get data from localstorage we have to use json.parse and json.stringify
-3. json.parse make object from json
-4. json.stringify make json from object
+1. local storage accept data in json format , so when ever we send or get data from localstorage we have to use json.parse and json.stringify
+1. json.parse make object from json
+1. json.stringify make json from object
--- 06_todoList_localStorage/script.js
+++ 06_todoList_localStorage/script.js
@@ -1,16 +1,13 @@
-let titleInput = document.querySelector('#titleInput');
+let input = document.querySelector('#titleInput');
 let desc = document.querySelector('#description');
 let box = document.querySelector('.box');
 
-let addbtn = document.querySelector('#addBtn');
-let deleteBtn = document.querySelector('#deleteBtn');
+let addBtn = document.querySelector('#addBtn');
 
 let task = localStorage.getItem('task') ? JSON.parse(localStorage.getItem('task')) : [];
+showTasks();
 
-showTask();
-
-
-function showTask  () {
+function showTasks() {
    task.forEach((value, index) => {
       let taskDiv = document.createElement('div');
       taskDiv.setAttribute('id', 'task');
@@ -19,52 +16,48 @@ function showTask  () {
       taskDiv.append(div);
 
       let title = document.createElement('h3');
-      title.textContent = value.title;
       let desc = document.createElement('h4');
-      desc.textContent = value.desc;
 
+      title.textContent = value.title;
+      desc.textContent = value.desc;
 
       div.append(title);
       div.append(desc);
 
-      let btn = document.createElement('button');
-      btn.setAttribute('id', 'deleteBtn');
-      btn.textContent = "-";
+      let deleteBtn = document.createElement('button');
+      deleteBtn.setAttribute('id', 'deleteBtn');
+      deleteBtn.textContent = "-";
 
-
-      btn.addEventListener('click', () => {
+      deleteBtn.addEventListener('click', () => {
          removeTask();
          task.splice(index, 1);
-         localStorage.setItem('task', JSON.stringify(task));//get all data from task array and display in local storage
+         localStorage.setItem('task', JSON.stringify(task));
+         showTasks();
 
-         showTask();
       });
-      taskDiv.append(btn);
+
+      taskDiv.append(deleteBtn);
       box.append(taskDiv);
 
-
    });
-};
+}
 
-function removeTask() { //this code will remove all the task div in html(dom)
+const removeTask = () => {
    task.forEach(() => {
       let div = document.querySelector('#task');
       div.remove();
-
    });
 
 };
 
-addbtn.addEventListener('click', () => {
+addBtn.addEventListener('click', () => {
    removeTask();
-   task.push({
-      title: titleInput.value,
+   task.push({ // will push user input into array []
+      title: input.value,
       desc: desc.value
    });
-   localStorage.setItem('task', JSON.stringify(task));//json.stringify so local storage can access its value
-   showTask();
-   titleInput.value = "";
+   localStorage.setItem('task', JSON.stringify(task));
+   showTasks();
+   input.value = "";
    desc.value = "";
-
-
 });
